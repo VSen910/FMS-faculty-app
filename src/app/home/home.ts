c3d9516c5a8a020f379b633dd624711c675dd790
Vineet Sen <imvsen02@gmail.com>
2025-09-08 22:10:35 +0530

    add change password modal

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Header } from '../components/header/header';
 import { FormCard } from '../components/form-card/form-card';
+import { Dialog } from '@angular/cdk/dialog';
+import { ChangePassword } from '../components/change-password/change-password';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,9 @@ import { FormCard } from '../components/form-card/form-card';
   styleUrl: './home.css'
 })
 export class Home {
+  constructor(private dialog: Dialog) {}
 
+  openChangePasswordDialog() {
+    this.dialog.open(ChangePassword);
+  }
 }
